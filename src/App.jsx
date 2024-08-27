b74b44c8e30c768a5aa3edf5967bca991ede3bc8
rCiDK <147745856+rCiDK@users.noreply.github.com>
2024-08-27 15:26:52 +0700

    intro done i think

--- src/App.jsx
+++ src/App.jsx
@@ -36,11 +36,6 @@ const App = () => {
         }, "+=.2")
         .to('.pace', { 
           duration: 1, 
-          rotate: 15,
-          ease: "power4.out" 
-        }, "-=3.1")
-        .to('.pace', { 
-          duration: 1, 
           height: "100%", 
           ease: "power4.out" 
         }, "-=3.1")
@@ -53,13 +48,30 @@ const App = () => {
           ease: "power4.out"
         }, 'p')
         .to('.title', { duration: 1, opacity: 1 }, '-=3')
-        .to('.title', { duration: 2.5, x: -1280, ease: CustomEase.create("custom", "M0,0 C0.419,0.019 0.473,0.449 0.527,0.567 0.578,0.681 0.62,1 1,1 ") }, '-=2.7');
+        .to('.title', { duration: 2.5, xPercent: -345, ease: CustomEase.create("custom", "M0,0 C0.419,0.019 0.473,0.449 0.527,0.567 0.578,0.681 0.62,1 1,1 ") }, '-=2.7')
+        .to('.pace', { 
+          duration: 1, 
+          height: "0%",
+          rotate: 90, 
+          ease: "power3.in" 
+        }, "-=1")
+        .to('.helloTitle', { 
+          duration: 1, 
+          opacity: 1, 
+          ease: "power4.in" 
+        }, "-=1")
+        .to('.title', { 
+          duration: 1, 
+          opacity: 0, 
+          ease: "power4.in" 
+        }, "-=1");
     });
   }, []);
 
   return (
     <>
       <div id='title-container'>
+        <h1 className='helloTitle'>Hello</h1>
         <h1 className="title">Halo • नमस्ते • こんにちは • สวัสดี • Olá • Hola • Salut • Привет • Ciao • Hello</h1>
       </div>
       <div id="preloader">
